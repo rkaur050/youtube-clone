04c9e9635ea19202ae5b309aeb62a878038a229c
rkaur050 <ramank25j@gmail.com>
2024-05-26 09:25:31 +0530

    done

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAAFU-h8I0v-eM_xP0lunst8sO48Fmce44",
+  apiKey: "process.env.REACT_APP_YT_API_KEY",
   authDomain: "yt-clone-2125.firebaseapp.com",
   projectId: "yt-clone-2125",
   storageBucket: "yt-clone-2125.appspot.com",
